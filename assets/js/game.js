1aa5f8a3b462d60099f04ad4202a7a296e2b2720
Adrian Duong Szeto <adrianduongszeto@gmail.com>
2022-04-06 21:08:48 -0700

    players dealt first damage to each other; followed by message log that state the events

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,33 @@
-// this creates a function named "fight"
-
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of qutation marks around playerName
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-console.log(playerName + " is ready for battle!");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("Your robot, " + playerName + ", has won!");
+var fight = function () {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-function fight () {
-    window.alert("The fight has begun!");
-}
+    //Subtract the value of 'plaayerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
 
-var name = "your name";
-console.log(name);
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
 
-// fight();
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the vlaue in the 'playerHealth' variable.
+        playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+        console.log(
+            enemyName +" attacked "+playerName+". "+playerName + " now has "+ playerHealth + " health remaining."
+        );
+};
+
+fight();
